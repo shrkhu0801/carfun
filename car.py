df4b58f05e6483c7cc1a7d15571afe872fca8f32
shrkhu0801 <shraddha8j@gmail.com>
2024-09-26 17:14:55 +0530

    Add files via upload

---
+++ car.py
@@ -0,0 +1,15 @@
+# Define the first function
+
+print("func1() is missing")
+
+
+print("this is extra text")
+
+# Define another function that calls fun1
+def fun2():
+    print("Calling fun1 from fun2...")
+    fun1()  # Call fun1 inside fun2gt
+    print("fun1 has been called!")
+
+# Call fun2 to execute the functions
+fun2()
